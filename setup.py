ca33048b862421ae73aa2ce93fd49ee4bc8dc6b0
Tyler <1tsantos7@gmail.com>
2018-04-08 16:19:52 -0400

    update db channels

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='pysosirius',
-	version='0.0.9',
+	version='0.1.0',
 	description='Unofficial Sirius XM API',
 	url='https://github.com/T-Santos/PySoSirius',
 	author='Tyler Santos',
